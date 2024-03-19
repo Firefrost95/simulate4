780d2ebe3bd217b2b08fe87846597a677ea9611a
Firefrost95 <jonassambaer@outlook.com>
2024-03-19 08:58:01 +0100

    arduino update

--- simulate4/src/main.cpp
+++ simulate4/src/main.cpp
@@ -1,18 +1,51 @@
 #include <Arduino.h>
+const int trigPin = 7; // Trigger Pin of Ultrasonic Sensor
+const int echoPin = 21; // Echo Pin of Ultrasonic Sensor
 
-// put function declarations here:
-int myFunction(int, int);
+long initialDistance = -1; // Variable to store the first measured distance
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+ Serial.begin(9600); // Starting Serial Terminal
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+ long duration, inches, cm; // Variables to make the calculation for measured distance.
+
+ pinMode(trigPin, OUTPUT);
+ digitalWrite(trigPin, LOW);
+ delayMicroseconds(2);
+ digitalWrite(trigPin, HIGH);
+ delayMicroseconds(10);
+ digitalWrite(trigPin, LOW);
+
+ pinMode(echoPin, INPUT);
+ duration = pulseIn(echoPin, HIGH);
+ inches = microsecondsToInches(duration);
+ cm = microsecondsToCentimeters(duration);
+
+ // Check if this is the first measurement
+ if (initialDistance == -1) {
+    initialDistance = cm; // Store the first measured distance
+ } else {
+    // Compare the current measurement to the initial measurement
+    if (cm == initialDistance) {
+      // Trigger a ping if the measurement matches the initial measurement
+      Serial.println("Ping!");
+    }
+ }
+
+ Serial.print(inches);
+ Serial.print(" in, ");
+ Serial.print(cm);
+ Serial.print(" cm");
+ Serial.println();
+ delay(100); // Delay 100ms before next reading.
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+long microsecondsToInches(long microseconds) {
+ return microseconds / 74 / 2;
+}
+
+long microsecondsToCentimeters(long microseconds) {
+ return microseconds / 29 / 2;
 }
